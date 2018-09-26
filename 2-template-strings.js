80ba60b6f5dab9a47e39020c043a9f6adeae0f43
shefy123 <38942966+shefy123@users.noreply.github.com>
2018-09-26 22:15:06 +0500

    Add files via upload

---
+++ 2-template-strings.js
@@ -0,0 +1,40 @@
+/*
+
+- First, refactor the code below to use variables. Then,
+- Refactor  to use Template Strings (backticks) instead on concatenation
+
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+ */
+
+
+let costOfApple = 10;
+let costOfMango = 20;
+let costOfBanana = 15;
+let costOfPeach = 20;
+
+console.log(`One apple costs Rs. ${costOfApple}`);
+console.log(`Two apples cost Rs. ${costOfApple * 2}`);
+console.log(`A dozen apples cost Rs. ${costOfApple * 12}`);
+console.log(`Six apples cost Rs. ${costOfApple * 6}`);
+console.log(`Ten apples cost Rs. ${costOfApple * 10}`);
+
+console.log(`One mango costs Rs. ${costOfMango}`);
+console.log(`Two mangoes cost Rs. ${ costOfMango* 2}`);
+console.log(`A dozen mangoes cost Rs. ${costOfMango * 12}`);
+console.log(`Six mangoes cost Rs. ${costOfMango * 6}`);
+console.log(`Ten mangoes cost Rs. ${costOfMango * 10}`);
+
+console.log(`One banana costs Rs. ${costOfBanana}`);
+console.log(`Two bananas cost Rs. ${costOfBanana * 2}`);
+console.log(`A dozen bananas cost Rs. ${costOfBanana * 12}`);
+console.log(`Six bananas cost Rs. ${costOfBanana * 6}`);
+console.log(`Ten bananas cost Rs. ${costOfBanana * 10}`);
+
+console.log(`One peach costs Rs. ${costOfPeach}`);
+console.log(`Two peaches cost Rs. ${costOfPeach * 2}`);
+console.log(`A dozen peaches cost Rs. ${costOfPeach * 12}`);
+console.log(`Six peaches cost Rs. ${costOfPeach * 6}`);
+console.log(`Ten peaches cost Rs. ${costOfPeach * 10}`);
+
+console.log(`one banana, one peach, one apple and one mango together cost Rs. ${costOfBanana + costOfMango + costOfApple + costOfPeach}`);
